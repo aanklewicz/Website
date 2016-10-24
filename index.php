867e899ae02b572e6bf47c3848744f147a20d1e1
Adam M. Anklewicz <adam@anklewicz.com>
2016-10-24 15:55:43 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<table width="100%" height="100%" border="1">
+	<tr height="85%">
+		<td valign="top" width="70%">Main Window</td>
+		<td valign="top" align="right" width="30%"><font size="12"><?php include 'clock.php'; ?></font><br /><?php include 'calendar.php'; ?></td>
+	</tr>
+	<tr height="15%">
+		<td colspan="2">CP24</td>
+	</tr>
+</table>
