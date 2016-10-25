68f3b07fbb0d40f758ec292959aa28ef1f635a4e
Adam M. Anklewicz <adam@anklewicz.com>
2016-10-25 09:49:24 -0400

    Commenting

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 
 <table width="100%" height="100%" border="1">
 	<tr height="85%">
+
+<!-- Main Window: This area will hold photos, text, and video to be cycled through. -->	
+
 		<td valign="top" width="70%">Main Window</td>
+		
+<!-- Clocks and Calendars: This area holds the calendar and the clock. It will also display what type of schedule the school is on. -->
+		
 		<td valign="top" align="right" width="30%"><font size="12"><?php include 'clock.php'; ?></font><br /><?php include 'calendar.php'; ?></td>
 	</tr>
+
+<!-- CP24: This row of the table displays a crawl of data similar to on 24 hour news programs. -->
+
 	<tr height="15%">
 		<td colspan="2">CP24</td>
 	</tr>
