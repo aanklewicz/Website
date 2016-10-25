6bef69a528ba3146739bc97e2ca599b6f656a496
Adam M. Anklewicz <adam@anklewicz.com>
2016-10-25 13:11:58 -0400

    Switch to MySQL

---
+++ connection/connection.php
@@ -0,0 +1,3 @@
+<!-- Calendars: iFrame embed code to show the school's calendar. -->
+
+<iframe src="https://calendar.google.com/calendar/embed?src=crestwood.on.ca_9llrelme8lk1i3ufg8bf4j047k%40group.calendar.google.com&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;hl=en_GB&amp;bgcolor=%23FFFFFF&amp;ctz=America%2FToronto" style="border-width:0" width="400" height="600" frameborder="0" scrolling="no"></iframe>
